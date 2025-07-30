7893a7b1e478811e6794219c621f570eefbcb6b0
Rafael Costrov <inov2@controller-oraculus.com.br>
2025-07-30 08:20:16 -0300

    Alterando o service drive para usar um usuario como subject

--- servico_google.py
+++ servico_google.py
@@ -5,16 +5,22 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 load_dotenv()
+
+
 def acessando_sheets():
     SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
     SCOPES = os.getenv('SCOPES_SHEETS').split(',')
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     client = gspread.authorize(creds)
     return client
 
+
 def acessando_drive():
     SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
+    EMAIL_USER = os.getenv('EMAIL_USER')
     SCOPES = os.getenv('SCOPES_DRIVE').split(',')
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES).with_subject(EMAIL_USER)
     drive_service = build("drive", "v3", credentials=creds)
     return drive_service
